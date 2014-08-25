2ef879bb8b2d98a1db73a3acc2ef979738686fce
Riley Spahn <rileyspahn@gmail.com>
2014-08-24 18:08:20 -0700

    -first commit

---
+++ generate_figures.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+
+from collections import Counter
+
+import matplotlib.pyplot as plt
+import os
+import sys
+
+
+def create_time_fig(in_file, out_file, title):
+    x_arr = []
+    min_y = []
+    avg_y = []
+    max_y = []
+    with open(in_file ,'r') as dat:
+        for line in dat:
+            if len(line) > 0:
+                sp = line.split(' ')
+                x = int(sp[0])
+                x_arr.append(x)
+                min_y.append(float(sp[1]))
+                avg_y.append(float(sp[2]))
+                max_y.append(float(sp[3]))
+
+    plt.xlabel("Time")
+    plt.ylabel("RTT (ms)")
+    plt.title(title)
+    #plt.plot(x_arr, min_y, label="Min")
+    plt.plot(x_arr, avg_y, label="Avg")
+    #plt.plot(x_arr, max_y, label="Max")
+    plt.legend(loc='upper left')
+    plt.savefig(out_file)
+    plt.clf()
+
+
+def get_cdf(dat):
+    x = []
+    y = []
+    curr = 0
+    total = len(dat)
+    counter = Counter(dat)
+
+    for c in iter(sorted(counter.iteritems())):
+        if c is None:
+            continue
+        curr += c[1]
+        x.append(float(curr)/total)
+        y.append(c[0])
+    return (x, y)
+        
+
+
+def create_cdf_fig(in_file, out_file, title):
+    min_y = []
+    avg_y = []
+    max_y = []
+    with open(in_file ,'r') as dat:
+        for line in dat:
+            if len(line) > 0:
+                sp = line.split(' ')
+                min_y.append(float(sp[1]))
+                avg_y.append(float(sp[2]))
+                max_y.append(float(sp[3]))
+    min_x, min__y = get_cdf(min_y)
+    avg_x, avg__y = get_cdf(avg_y)
+    max_x, max__y = get_cdf(max_y)
+
+    ax = plt.subplot(1, 1, 1)
+    plt.plot(min_x, min__y, label="Min")
+    plt.plot(avg_x, avg__y, label="Avg")
+    plt.plot(max_x, max__y, label="Max")
+
+    plt.legend(loc='upper left')
+
+    plt.xlabel("Fraction")
+    plt.ylabel("RTT (ms)")
+    plt.title(title)
+    plt.savefig(out_file)
+    plt.clf()
+
+def get_out_file(in_file):
+    return in_file.split('.')[0] + ".png"
+
+
+def get_title(in_file):
+    return in_file.split('.')[0]
+
+if len(sys.argv) != 3:
+    sys.exit(1)
+
+in_file = sys.argv[1]
+out_dir = sys.argv[2]
+title = get_title(in_file)
+time_file = "time_" + get_out_file(in_file)
+cdf_file = "cdf_" + get_out_file(in_file)
+
+create_time_fig(in_file, os.path.join(out_dir, time_file), title + " Average Ping Time")
+create_cdf_fig(in_file, os.path.join(out_dir, cdf_file), title + " CDF")
---
+++ pingstat.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Riley Spahn
+# 08/24/14
+
+get_ping_stat () {
+    if [[ -z "$1" || -z "$2" ]] ; then 
+        return 1
+    fi
+    target=$1
+    pings=$2
+    epoch=$(date +%s)
+    pstat=$(ping -c $pings $target -q | grep "^rtt" | cut -d" " -f4 | sed "s/\// /g")
+    echo "$epoch $pstat"
+    return 0
+}
+
+
+update_dats () {
+    PINGS=5
+    stat_file=$1
+    if [[ -z "$stat_file" ]] ; then 
+        return 1
+    fi
+
+    for line in $( cat $stat_file); do
+        url=$(echo $line | cut -d"," -f1)
+        outfile=$(echo $line | cut -d"," -f2)
+        echo "Testing: $url"
+        get_ping_stat $url $PINGS >> $outfile
+    done
+}
+
+generate_figs () {
+    out_dir=$1
+
+    for dat_file in $( ls *.dat); do
+        echo $dat_file
+        python generate_figures.py $dat_file $out_dir
+    done
+}
+
+generate_html() {
+    out_dir=$1
+    index_file="$out_dir/index.html"
+    tmp_file=".index.html.bak"
+
+    echo "<html><title>Ping Stat</title><b>Ping Stat</b>" >> $tmp_file
+
+    for img in $( ls $out_dir/*.png ); do
+        echo $img
+        img_file=$( echo $img | cut -d"/" -f2 )
+        echo $img_file
+        echo "<p><img src=\"$img_file\"/></p>" >> $tmp_file
+    done
+    echo "</html>" >> $tmp_file
+    mv $tmp_file $index_file
+}
+
+out_dir="out"
+mkdir -p $out_dir
+
+# Ping all the things based on the input file
+update_dats $1
+
+# Generate the new figures.
+generate_figs $out_dir
+
+# Regenerate HTML.
+echo $out_dir
+generate_html out out/figs
